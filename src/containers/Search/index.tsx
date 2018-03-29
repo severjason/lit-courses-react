13d1346a52d5ed6edccbe718c19eafbbd20d06b7
severjason <ssever2010@gmail.com>
2018-03-30 01:50:58 +0300

    Added react-router and react-helmet

--- src/containers/Search/index.tsx
+++ src/containers/Search/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Footer } from '../../components';
 import { SearchForm, TrackList } from '../../components';
+import { Helmet } from 'react-helmet';
 
 class Search extends React.Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>Tracks search app</title>
+                </Helmet>
                 <div className="ui container app-container">
                     <SearchForm/>
                     <TrackList/>
