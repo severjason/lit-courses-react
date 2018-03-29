0ab7db3a233bccda89ec0b7014b7e51f57f67f0b
severjason <ssever2010@gmail.com>
2018-03-30 01:49:51 +0300

    Routing added

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import './index.css';
-import Search from '../Search';
+import { Search } from '../';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Search/>
-    );
-  }
-}
+const App: React.StatelessComponent<{}> = () => {
+  return (
+      <Switch>
+        <Route path="/search" component={Search}/>
+        <Route exact={true} path="/" render={() => <Redirect to="/search"/>} />
+        <Route path="*" render={() => <Redirect to="/search"/>} />
+      </Switch>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import App from './containers/App';
+import { App } from './containers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <BrowserRouter>
+                <App/>
+            </BrowserRouter>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
