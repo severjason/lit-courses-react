21f1d12cad14728526ede27ee4a43a60c0e4b837
severjason <ssever2010@gmail.com>
2018-03-29 17:30:44 +0300

    Added basic App container

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './index.css';
+import Search from '../Search';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Search/>
+    );
+  }
+}
+
+export default App;
