3d55c09d1b882c77399d7178ee1219a7e9ada10f
severjason <ssever2010@gmail.com>
2018-03-30 18:59:48 +0300

    Added thunkMiddleware and loggerMiddleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, Store, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'semantic-ui-css/semantic.min.css';
 import { App } from './containers';
+import rootReducer, { State } from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const loggerMiddleware = createLogger();
+
+const store: Store<State> = createStore(
+    rootReducer,
+    applyMiddleware (
+        thunkMiddleware,
+        loggerMiddleware
+    )
+);
+
 ReactDOM.render(
-    <BrowserRouter>
-                <App/>
-            </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
