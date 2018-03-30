304d17af0b963ac0bd92db2a9f7fa1e4cc9b94c6
severjason <ssever2010@gmail.com>
2018-03-30 18:57:54 +0300

    Search page modification from class to const+connect

--- src/containers/SearchPage/index.tsx
+++ src/containers/SearchPage/index.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react';
 import { SearchForm } from '../';
+import { State } from '../../reducers';
+import * as actions from '../../actions';
 import { Footer, TrackList } from '../../components';
 import { Helmet } from 'react-helmet';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 
-class SearchPage extends React.Component {
-    render() {
-        return (
-            <div>
-                <Helmet>
-                    <title>Tracks search app</title>
-                </Helmet>
-                <div className="ui container app-container">
-                    <SearchForm/>
-                    <TrackList/>
-                </div>
-                <Footer/>
-            </div>
-        );
-    }
-}
+const SearchPage = (props: {}) => (
+    <div>
+        <Helmet>
+            <title>Tracks search app</title>
+        </Helmet>
+        <div className="ui container app-container">
+            <SearchForm {...props}/>
+            <TrackList/>
+        </div>
+        <Footer/>
+    </div>
+);
 
-export default SearchPage;
+export default connect<any, Dispatch<any>>(
+    (state: State) => (state.isLoading),
+    (dispatch: Dispatch<any>) => ({
+        actions: bindActionCreators(actions, dispatch)
+    })
+)(SearchPage);
