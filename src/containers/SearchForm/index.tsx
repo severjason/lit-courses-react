5235833d742a8a80f1d93eac00680f714d8cde05
severjason <ssever2010@gmail.com>
2018-03-30 18:59:26 +0300

    Added loading support when submit

--- src/containers/SearchForm/index.tsx
+++ src/containers/SearchForm/index.tsx
@@ -2,15 +2,31 @@ import * as React from 'react';
 import './index.css';
 import { ChangeEvent, ReactNode } from 'react';
 
-class SearchForm extends React.Component {
+class SearchForm extends React.Component<any, any> {
 
     state = {
-        value: ''
+        value: '',
+        isLoading: this.props.isLoading
     };
+
+    componentDidUpdate() {
+        const { isLoading } = this.props;
+
+        if (this.state.isLoading !== isLoading) {
+            this.setState({isLoading: isLoading});
+        }
+    }
 
     private onChange = (e: ChangeEvent<HTMLInputElement>): void => this.setState({value: e.target.value});
 
     private isEmpty = (): boolean => !this.state.value;
+
+    private handleClick = (e: any) => {
+        const { actions } = this.props;
+        e.preventDefault();
+        actions.fetchTracks(this.state.value);
+        this.setState({value: ''});
+    }
 
     public render(): ReactNode {
         return (
@@ -20,11 +36,16 @@ class SearchForm extends React.Component {
                         onChange={this.onChange}
                         className="app-search-input"
                         type="text"
+                        value={this.state.value}
                         placeholder="Search tracks by artist..."
                     />
                     <button
+                        onClick={this.handleClick}
                         disabled={this.isEmpty()}
-                        className={'ui button app-search-button'}
+                        className={
+                            (this.state.isLoading)
+                                ? 'ui button app-search-button loading'
+                                : 'ui button app-search-button'}
                     >
                         <i className="search icon"/>
                     </button>
