3de97027e07b25a1d62eaf24895fc850269059e6
severjason <ssever2010@gmail.com>
2018-03-31 15:38:11 +0300

    Added ErrorComponent

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,11 @@
 import Footer from './Footer';
 import TrackList from './TrackList';
+import ErrorComponent from './ErrorComponent';
+import SearchResponse from './SearchResponse';
 
 export {
     Footer,
     TrackList,
+    ErrorComponent,
+    SearchResponse,
 };
