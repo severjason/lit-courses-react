921403c70f380d10ef35aa5273a05bed431a1f68
severjason <ssever2010@gmail.com>
2018-03-31 15:34:02 +0300

    Updated interfaces and added mapTracks function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,24 +1,26 @@
 import { types } from '../constants/types';
-import { Action } from '../models';
 import Api from '../services/api';
 import { Dispatch } from 'redux';
+import { ActionI, TrackI, ApiResponseI, ActionFetchI } from '../interfaces';
 
 const api = new Api();
 
-export const requestTracks = (): Action => ({
+export const requestTracks = (): ActionI => ({
     type: types.REQUEST_TRACKS,
     isLoading: true,
 });
 
-export const tracksFetched = (json: JSON): Action => ({
+export const tracksFetched = (json: TrackI[]): ActionFetchI => ({
     type: types.TRACKS_FETCHED,
     isLoading: false,
+    received: true,
     payload: json,
 });
 
-export const tracksFetchError = (): Action => ({
+export const tracksFetchError = (error: Error): ActionI => ({
     type: types.TRACKS_FETCH_ERROR,
     isLoading: false,
+    error: error,
 });
 
 export function fetchTracks(artist: any) {
@@ -26,13 +28,29 @@ export function fetchTracks(artist: any) {
         dispatch(requestTracks());
         return api.getTracks(artist)
             .then((response: Response) => response.json())
-            .then((json: JSON) => {
-                console.log(json);
-                dispatch(tracksFetched(json));
+            .then((json: ApiResponseI) => {
+                dispatch(tracksFetched(mapTracks(json.results)));
             })
-            .catch((error) => {
-                console.log(error);
-                dispatch(tracksFetchError());
+            .catch((error: Error) => {
+                dispatch(tracksFetchError(error));
             });
     };
+}
+
+function mapTracks(tracksArray: any[]): TrackI[] {
+    return tracksArray.map((track: TrackI) => {
+        return {
+            trackId: track.trackId,
+            artistName: track.artistName,
+            collectionName: track.collectionName,
+            collectionPrice: track.collectionPrice,
+            artworkUrl100: track.artworkUrl100,
+            primaryGenreName: track.primaryGenreName,
+            opened: false,
+            trackName: track.trackName,
+            trackCount: track.trackCount,
+            trackPrice: track.trackPrice,
+            trackTimeMillis: track.trackTimeMillis
+        };
+    });
 }
